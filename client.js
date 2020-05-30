0663ca17e3d4b0596765af00e9190b1df34c30cf
VedantBang <vedantbang5@gmail.com>
2020-05-30 15:27:26 +0530

    327

--- client.js
+++ client.js
@@ -10,17 +10,20 @@ const rl = readline.createInterface({
 
 rl.question("Enter a nickname: ", function(answer) {
 	const nick = answer || ("user"+((Math.floor(Math.random()*10000)).toString()));
+
 	console.log(`Welcome to Chat Server, powered by WebSockets`);
 	console.log(`Connected as ${nick}`);
 	console.log("Type your messages directly into process.stdin");
+
+	ws.on('message', function incoming(data) {
+  		console.log(data);
+	});
+
 	ws.on('open', function open() {
   		rl.on('line', function getData(data){
   			ws.send(`[${nick}]: ${data}`);
   		});
 	});
 
-	ws.on('message', function incoming(data) {
-  		console.log(data);
-	});
 });
  
