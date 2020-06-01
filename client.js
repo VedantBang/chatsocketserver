d200dec7e366ff95cd0dd9c88d1818dccc66130f
VedantBang <vedantbang5@gmail.com>
2020-06-01 12:32:12 +0530

    comments in code

--- client.js
+++ client.js
@@ -1,9 +1,11 @@
+// interface for taking user input
 const readline = require('readline');
 const rl = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout
 });
 
+// creating the client
 const WebSocketClient = require('websocket').client;
 const client = new WebSocketClient();
 
@@ -11,38 +13,55 @@ let username;
 
 const commands = require('./commands/client');
 
+// set the prompt to '>' 
 rl.setPrompt('>');
+// preserve position of cursor
 const preserveCursor = true;
 
+// handle connection failure
 client.on('connectFailed', error => {
 	console.log(`[ERROR] Error while connecting: ${error.toString()}`);
 });
 
+// on connecting
 client.on('connect', conn => {
+	// handle errors in connections
 	conn.on('error', error => {
 		console.log(`\n[ERROR] Error in connection: ${error.toString()}`);
 	});
+	// handle disconnect
 	conn.on('close', () => {
 		console.log('\nDisconnected');
+		process.exit(0);
 	});
+	// on receving message
 	conn.on('message', msg => {
+		// print message and preserve pending user input
 		if(msg.type === 'utf8') console.log(`\n${msg.utf8Data}`);
 		rl.prompt({ preserveCursor });
 	});
+	// on pressing Ctrl+c
 	rl.on('SIGINT', () => {
+		// close the connection and the application
 		conn.close();
 		console.log('\nDisconnected');
 		process.exit(0);
 	});
+	// on user input
 	rl.on('line', data => {
+		// check if input is command, do appropriate action
 		if(commands.hasOwnProperty(data)) commands[data](conn);
+		// otherwise send message
 		else conn.send(`[${username}] ${data}`);
 		rl.prompt({ preserveCursor });
 	});	
 });
 
+// ask for username for connecting
 rl.question('Enter a nickname: ', answer => {
+	// if not provided, pick random
 	username = answer || (`user${Math.floor(Math.random() * 100)}`);
+	// get the hostname from command-line arguments, or default to the server running on herokuapp
 	let context = process.argv[2] || 'heroku';
 	let url;
 	switch(context){
@@ -53,6 +72,7 @@ rl.question('Enter a nickname: ', answer => {
 			url = `ws://${context}:8765`;
 			break;
 	}
+	// connect to the server, passing in username in headers
 	client.connect(url,null,null,{ username });
 });
 
--- server.js
+++ server.js
@@ -14,30 +14,38 @@ try{
 	process.exit(0);
 }
 
-
+// creating the server
 const wss = new WebSocketServer({ httpServer });
 const commands = require('./commands/server');
 const setUsername = require('./functions/setUsername');
 
+// on receiving new connection request
 wss.on('request', req => {
+	// accept the connection
 	const conn = req.accept(null, req.origin);
+	// middleware - get the username from request header and attach to connection
 	setUsername(req,conn);
+	// log messages
 	console.log(`[NETWORK] Connected to ${conn.remoteAddress}`);
 	conn.sendUTF('[SERVER] Connection estabilished');
+	// notify all other connections of new connection
 	wss.connections.forEach( client => {
 		if( client.remoteAddress !== conn.remoteAddress ) client.sendUTF(`[SERVER] ${conn.username} has joined`);
 	});
-
+	// handle disconnect
 	conn.on('close', (code,desc) => {
+		// log message
 		console.log(`[NETWORK] ${conn.remoteAddress} disconnected`);
+		// notify all other connections of disconnect
 		wss.connections.forEach( client => {
 			if( client.remoteAddress !== conn.remoteAddress ) client.sendUTF(`[SERVER] ${conn.username} has disconnected`);
 		});
 	});
-
+	// on receiving message from a connection
 	conn.on('message', msg => {
+		// log message
 		console.log(`[MESSAGE] ${msg.utf8Data}`);
-
+		// relay to all other connections
 		wss.connections.forEach( client => {
 			if( client.remoteAddress !== conn.remoteAddress ) client.sendUTF(msg.utf8Data);
 		});
